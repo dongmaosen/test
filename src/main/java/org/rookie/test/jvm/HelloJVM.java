d44384e8f07053820903cca66a2a03dfc619e35f
dongmaosen <dongmaosen@yeah.net>
2020-01-08 20:04:09 +0800

    11

---
+++ src/main/java/org/rookie/test/jvm/HelloJVM.java
@@ -0,0 +1,10 @@
+package org.rookie.test.jvm;
+
+public class HelloJVM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi, call me jvm !");
+	}
+
+}
